57a091539423193e1706043dbe7ab907742dc812
Stefan Kebekus <kebekus@users.noreply.github.com>
2026-02-06 16:11:47 +0100

    Singletons

--- Mathlib/Analysis/Complex/ValueDistribution/LogCounting/Basic.lean
+++ Mathlib/Analysis/Complex/ValueDistribution/LogCounting/Basic.lean
@@ -125,6 +125,28 @@ Evaluation of the logarithmic counting function at zero yields zero.
   simp [logCounting]
 
 /--
+The logarithmic counting function of a singleton indicator is asymptotically equal to
+`log · - log ‖e‖`.
+-/
+@[simp] lemma logCounting_single_eq_log_sub_const [ProperSpace E] {e : E} {r : ℝ} (hr : ‖e‖ ≤ r) :
+    logCounting (single e) r = log r - log ‖e‖ := by
+  simp only [logCounting, AddMonoidHom.coe_mk, ZeroHom.coe_mk]
+  rw [finsum_eq_sum_of_support_subset _ (s := (finite_singleton e).toFinset)
+    (by simp_all [toClosedBall, restrict_apply, single_eval])]
+  simp only [toFinite_toFinset, toFinset_singleton, Finset.sum_singleton]
+  rw [toClosedBall_eval_within _ (by simpa [abs_of_nonneg ((norm_nonneg e).trans hr)])]
+  by_cases he : 0 = e
+  · simp [← he, single_eval]
+  · simp only [single_eval, he, reduceIte, Int.cast_zero, zero_mul, add_zero,
+      log_mul (ne_of_lt (lt_of_lt_of_le (norm_pos_iff.mpr (he ·.symm)) hr)).symm
+      (inv_ne_zero (norm_ne_zero_iff.mpr (he ·.symm))), log_inv]
+    grind
+
+/-!
+### Elementary Properties of Logarithmic Counting Functions
+-/
+
+/--
 The logarithmic counting function is even.
 -/
 lemma logCounting_even [ProperSpace E] (D : locallyFinsupp E ℤ) :
@@ -167,6 +189,25 @@ lemma logCounting_mono [ProperSpace E] {D : locallyFinsupp E ℤ} (hD : 0 ≤ D)
       · simp_all only [abs_of_pos ha, mem_closedBall, dist_zero_right, abs_of_pos hb]
         linarith
   · exact Int.cast_nonneg (hD 0)
+
+/--
+The logarithmic counting function of a positive function with locally finite support is
+asymptotically strictly monotone.
+-/
+lemma logCounting_strictMono [ProperSpace E] {D : locallyFinsuppWithin (univ : Set E) ℤ} {e : E}
+    (hD : single e ≤ D) :
+    StrictMonoOn (logCounting D) (Ioi ‖e‖) := by
+  rw [(by aesop : logCounting D = logCounting (single e) + logCounting (D - single e))]
+  apply StrictMonoOn.add_monotone
+  · intro a ha b hb hab
+    rw [mem_Ioi] at ha hb
+    rw [logCounting_single_eq_log_sub_const ha.le, logCounting_single_eq_log_sub_const hb.le]
+    gcongr
+    exact (norm_nonneg e).trans_lt ha
+  · intro a ha b hb hab
+    apply logCounting_mono _ _ ((norm_nonneg e).trans_lt hb) hab
+    · simp [hD]
+    · simpa [mem_Ioi] using (norm_nonneg e).trans_lt ha
 
 /--
 For `1 ≤ r`, the logarithmic counting function is non-negative.
--- Mathlib/Topology/LocallyFinsupp.lean
+++ Mathlib/Topology/LocallyFinsupp.lean
@@ -97,6 +97,33 @@ lemma coe_injective [Zero Y] :
     Injective (· : locallyFinsuppWithin U Y → X → Y) := DFunLike.coe_injective
 
 /-!
+## Singleton Indicators as Functions with Locally Finite Support
+-/
+
+/--
+Is analogy to `Finsupp.single`, this definition presents the indicator function
+of a single point as a function with locally finite support.
+-/
+noncomputable def single (x : X) : locallyFinsuppWithin (Set.univ : Set X) ℤ where
+  toFun := by
+    classical
+    exact (if · = x then 1 else 0)
+  supportWithinDomain' z hz := by tauto
+  supportLocallyFiniteWithinDomain' := by
+    intro _ _
+    use ⊤
+    simp only [top_eq_univ, univ_mem, univ_inter, true_and]
+    convert (finite_singleton x)
+    aesop
+
+open Classical in
+/--
+Simplifier lemma: `single e` takes the value `1` at `e` and is zero otherwise.
+-/
+@[simp] lemma single_eval {x₁ x₂ : X} :
+    single x₁ x₂ = if x₂ = x₁ then 1 else 0 := rfl
+
+/-!
 ## Elementary properties of the support
 -/
 
@@ -392,6 +419,32 @@ theorem nsmul_negPart (n : ℕ) (f : locallyFinsuppWithin U Y) : (n • f)⁻ = 
   by_cases h : -f x < 0
   · simpa [max_eq_right_of_lt h] using nsmul_le_nsmul_right h.le n
   · simpa [not_lt.1 h] using nsmul_nonneg (not_lt.1 h) n
+
+/--
+The function `single e` is positive.
+-/
+@[simp] lemma single_pos {x : X} : 0 < single x := by
+  apply lt_of_le_of_ne
+  · intro y
+    by_cases he : y = x
+    all_goals
+      simp_all [single_eval]
+  · apply DFunLike.ne_iff.2
+    use x
+    simp [single_eval]
+
+/--
+Every positive function with locally finite supports dominates a singleton indicator.
+-/
+lemma exists_single_le_pos {D : locallyFinsuppWithin (Set.univ : Set X) ℤ} (h : 0 < D) :
+    ∃ e, single e ≤ D := by
+  obtain ⟨z, hz⟩ := (by simpa [D.ext_iff] using (ne_of_lt h).symm : ∃ z, D z ≠ 0)
+  use z
+  intro e
+  by_cases he : e = z
+  · subst he
+    simpa [single_eval] using Int.lt_iff_le_and_ne.mpr ⟨h.le e, hz.symm⟩
+  · simpa [he, single_eval] using h.le e
 
 end LinearOrder
 
